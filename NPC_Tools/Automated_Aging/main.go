2b3cd876d35d359785c56fa37fd701377477c3cb
Kyle <kyle@Kyles-MacBook-Pro.local>
2018-07-31 14:59:38 -0700

    finished v1, program is functional

--- NPC_Tools/Automated_Aging/main.go
+++ NPC_Tools/Automated_Aging/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"math/rand"
 	"os"
 	"strconv"
@@ -10,11 +11,12 @@ import (
 	"time"
 )
 
-var year, yearInt, yearBorn, currentAge, personalModifiers int
-var appearantAge, intAge, perAge, strAge, staAge, preAge, comAge, dexAge, quiAge, decrepitude int
-var intelligence, perception, strength, stamina, presence, communication, dexterity, quickness int
-var globalBonus, name string
+var appearantAge, year, yearInt, yearBorn, effectiveAge int
+var globalBonus, decrepitude, personalModifiers int
+var name, virtues string
+var sfb, ua, aq, crisis = "n", "n", "n", ""
 var advanceYear, view, older = "n", "n", "n"
+var filename = "Aging Chart Template.csv"
 
 func ageCalculation() {
 	rollMult := 1
@@ -23,8 +25,8 @@ func ageCalculation() {
 		rollMult++
 		ageRoll = diceRoll(10)
 	}
-	bonusInt, _ := strconv.Atoi(globalBonus)
-	ageTotal := ageRoll*rollMult + bonusInt + personalModifiers
+	agePenalty := effectiveAge / 10
+	ageTotal := ageRoll*rollMult + globalBonus + personalModifiers + agePenalty
 
 	//fmt.Print("Die roll: ", ageRoll, "\n")
 	//fmt.Print("Age Multiplier: ", rollMult, "\n")
@@ -35,77 +37,87 @@ func ageCalculation() {
 	switch {
 	case ageTotal <= 2:
 		fmt.Print(name, " has experienced no adverse effects of age.\n")
-	case ageTotal < 10:
+	case ageTotal <= 9:
 		fmt.Print(name, " looks a little older.\n")
 		older = "y"
 		appearantAge++
-	case ageTotal < 13:
+	case ageTotal <= 12:
 		older = "y"
+		fmt.Print(name, " looks a little older.\n")
 		appearantAge++
 		decrepitude++
-		// add 1 to any aging stat
 	case ageTotal == 13:
 		older = "y"
+		fmt.Print(name, " has experienced a crisis.\n")
 		appearantAge++
-		// gain sufficient aging points in any 1 characteristic to reach
-		// the next level of decrepitude and crisis
-	case ageTotal == 14:
+		switch {
+		case decrepitude < 5:
+			decrepitude = 5
+		case decrepitude < 15:
+			decrepitude = 15
+		case decrepitude < 30:
+			decrepitude = 30
+		case decrepitude < 50:
+			decrepitude = 50
+		case decrepitude < 75:
+			decrepitude = 75
+		}
+		crisisCalculation()
+	case ageTotal <= 17:
 		older = "y"
+		fmt.Print(name, " looks a little older.\n")
 		appearantAge++
-		quiAge++
 		decrepitude++
-	case ageTotal == 15:
+	case ageTotal <= 21:
 		older = "y"
+		fmt.Print(name, " looks a little older.\n")
 		appearantAge++
-		staAge++
-		decrepitude++
-	case ageTotal == 16:
-		older = "y"
-		appearantAge++
-		perAge++
-		decrepitude++
-	case ageTotal == 17:
-		older = "y"
-		appearantAge++
-		preAge++
-		decrepitude++
-	case ageTotal == 18:
-		older = "y"
-		appearantAge++
-		staAge++
-		strAge++
-		decrepitude++
-		decrepitude++
-	case ageTotal == 19:
-		older = "y"
-		appearantAge++
-		dexAge++
-		quiAge++
-		decrepitude++
-		decrepitude++
-	case ageTotal == 20:
-		older = "y"
-		appearantAge++
-		comAge++
-		preAge++
-		decrepitude++
-		decrepitude++
-	case ageTotal == 21:
-		older = "y"
-		appearantAge++
-		intAge++
-		perAge++
 		decrepitude++
 		decrepitude++
 	case ageTotal > 21:
 		older = "y"
+		fmt.Print(name, " has experienced a crisis.\n")
 		appearantAge++
-		// gain sufficient aging points in any 1 characteristic to reach
-		// the next level of decrepitude and crisis
+		switch {
+		case decrepitude < 5:
+			decrepitude = 5
+		case decrepitude < 15:
+			decrepitude = 15
+		case decrepitude < 30:
+			decrepitude = 30
+		case decrepitude < 50:
+			decrepitude = 50
+		case decrepitude < 75:
+			decrepitude = 75
+		}
+		crisisCalculation()
+	}
+}
 
-	default:
-		fmt.Print("Bad news for ", name, "\n")
-		older = "y"
+func crisisCalculation() {
+	decrepitudeScore := math.Floor((math.Sqrt(8*(float64(decrepitude)/5)+1) - 1) / 2)
+	fmt.Print("\nDECREPTIUDE SCORE: ", decrepitudeScore, "\n")
+	crisisRoll := diceRoll(10)
+	agePenalty := effectiveAge / 10
+	crisisTotal := crisisRoll + agePenalty + int(decrepitudeScore)
+	switch {
+	case crisisTotal <= 14:
+		if ua == "y" {
+			crisis = "Crisis prevented due to Unaging"
+		} else {
+			crisis = "Minor sickness, a season of work is lost"
+		}
+	case crisisTotal == 15:
+		crisis = "Minor illness. Stamina roll of 3+ or CrCo20 to survive"
+	case crisisTotal == 16:
+		crisis = "Serious illness. Stamina roll of 6+ or CrCo25 to survive"
+	case crisisTotal == 17:
+		crisis = "Major illness. Stamina roll of 9+ or CrCo30 to survive"
+	case crisisTotal == 18:
+		crisis = "Critical illness. Stamina roll of 12+ or CrCo35 to survive"
+	case crisisTotal >= 19:
+		crisis = "Terminal illness. CrCo40 required to survive"
+
 	}
 }
 
@@ -143,10 +155,12 @@ func check(e error) {
 
 func main() {
 	//file, err := os.Open("ages.txt")
-	file, err := os.Open("Keras Nisi Aging Chart Test - Sheet1.csv")
+	//file, err := os.Open("Keras Nisi Aging Chart Test - Sheet1.csv")
+	file, err := os.Open(filename)
 
 	//input, err := ioutil.ReadFile(file)
-	input, err := ioutil.ReadFile("Keras Nisi Aging Chart Test - Sheet1.csv")
+	//input, err := ioutil.ReadFile("Keras Nisi Aging Chart Test - Sheet1.csv")
+	input, err := ioutil.ReadFile(filename)
 
 	// close file on exit and check for its returned error
 	defer func() {
@@ -179,21 +193,23 @@ func main() {
 
 	// extract each individual line
 	lines := strings.Split(string(input), "\n")
-	//Name,Year Born,Age,Personal Modifiers,Intelligence Aging,Perception Aging,Strength Aging,Stamina Aging,Prescence Aging,Communication Aging,Dexterity Aging,Quickness Aging,Decrepitude Score
 	for _, line := range lines {
 		// Displays aging sheet for viewing
 		words := strings.Split(string(line), ",")
-		//fmt.Print("!!! TEST !!!\n", words[1:], "\n!!! TEST !!!\n")
+		sfb, ua, aq, crisis = "n", "n", "n", ""
+		older = "n"
 
-		arrayInt := make([]int, len(words[1:]))
-		for i := range arrayInt {
-			arrayInt[i], _ = strconv.Atoi(words[i+1])
+		var arrayInt []int
+		if len(words) > 1 {
+			arrayInt = make([]int, len(words[2:]))
+			for i := range arrayInt {
+				arrayInt[i], _ = strconv.Atoi(words[i+1])
+			}
 		}
-		fmt.Println("!!! TEST ARRAY !!!\n", arrayInt)
 
 		if words[0] == "Living Modifiers" {
 			words[0] = "\nLiving Modifiers"
-			globalBonus = words[1]
+			globalBonus, _ = strconv.Atoi(words[1])
 			output := strings.Join(words, ",")
 			if _, err = fo.WriteString(output); err != nil {
 				panic(err)
@@ -201,68 +217,71 @@ func main() {
 			if _, err = fo.WriteString("\n"); err != nil {
 				panic(err)
 			}
+			continue
 		} else if words[0] == "Year" {
-			yearString := words[1]
-			size := len(yearString)
-			year, _ = strconv.Atoi(yearString[:size-1])
 			if advanceYear == "y" {
-				year++
-				words[1] = strconv.Itoa(year)
+				grabYear++
+				words[1] = strconv.Itoa(grabYear)
 			}
 			output := strings.Join(words, ",")
 			if _, err = fo.WriteString(output); err != nil {
 				panic(err)
 			}
-		} else if words[0] == "Name" {
 			continue
-			//} else if words[12] != "" {
-		} else if len(line) > 25 {
-			// This line is meant to detect lines that are characters
+		} else if words[0] == "Name" {
+			output := strings.Join(words, ",")
+			if _, err = fo.WriteString(output); err != nil {
+				panic(err)
+			}
+			if _, err = fo.WriteString("\n"); err != nil {
+				panic(err)
+			}
+			continue
+		} else if line != "" {
 			name = words[0]
-			//yearBorn, _ = strconv.Atoi(words[1])
-			yearBorn = arrayInt[0]
-			currentAge = year - yearBorn
-			//appearantAge = words[3]
-			appearantAge = arrayInt[2]
-			//personalModifiers, _ = strconv.Atoi(words[4])
-			personalModifiers = arrayInt[3]
-			//intAge = words[5]
-			intAge = arrayInt[4]
-			//perAge = words[6]
-			perAge = arrayInt[5]
-			//strAge = words[7]
-			strAge = arrayInt[6]
-			//staAge = words[8]
-			staAge = arrayInt[7]
-			//preAge = words[9]
-			preAge = arrayInt[8]
-			//comAge = words[10]
-			comAge = arrayInt[9]
-			//dexAge = words[11]
-			dexAge = arrayInt[10]
-			//quiAge = words[12]
-			quiAge = arrayInt[11]
-			//decrepitude = words[13]
-			decrepitude = arrayInt[12]
-			//fmt.Print("Testing: \n")
-			//fmt.Print("Int Aging: ", intAge, "\n")
-			//fmt.Print("perAge: ", perAge, "\n")
-			//fmt.Print("Str Age:", strAge, "\n")
-			//fmt.Print("staAge:", staAge, "\n")
-			//fmt.Print("preAge: ", preAge, "\n")
-			//fmt.Print("com age:", comAge, "\n")
-			//fmt.Print("dexAge: ", dexAge, "\n")
-			//fmt.Print("quiAge: ", quiAge, "\n")
-			//fmt.Print("decrepitude: ", decrepitude, "\n")
+			virtues = words[1]
+			if strings.Contains(virtues, "FB") {
+				if strings.Contains(virtues, "SFB") {
+					personalModifiers = -3
+					sfb = "y"
+				} else {
+					personalModifiers = -1
+				}
+			}
+			if strings.Contains(virtues, "UA") {
+				ua = "y"
+			}
+			if strings.Contains(virtues, "AQ") {
+				aq = "y"
+			}
+			yearBorn = arrayInt[1]
+			effectiveAge = arrayInt[2]
+			fmt.Print("\nEffective Age of ", name, " is: ", effectiveAge, "\n\n")
+			words[3] = strconv.Itoa(effectiveAge)
+			appearantAge = arrayInt[3]
+			decrepitude = arrayInt[4]
 
 			if advanceYear == "y" {
-				if currentAge >= 35 {
-					fmt.Print("Current Age: ", currentAge, "\n")
+				effectiveAge++
+				words[3] = strconv.Itoa(effectiveAge)
+				if effectiveAge >= 35 && sfb == "n" || effectiveAge >= 50 {
+					fmt.Print("Current Age: ", effectiveAge, "\n")
 					ageCalculation()
+					if aq == "y" {
+						ageCalculation()
+						fmt.Print(name, " appears to be aging quicker \n")
+						effectiveAge++
+					}
 					if older == "y" {
-						oldAge, _ := strconv.Atoi(words[3])
+						oldAge, _ := strconv.Atoi(words[4])
 						oldAge++
-						words[3] = strconv.Itoa(oldAge)
+						if aq == "y" {
+							oldAge++
+						}
+						words[3] = strconv.Itoa(effectiveAge)
+						words[4] = strconv.Itoa(oldAge)
+						words[5] = strconv.Itoa(decrepitude)
+						words[6] = crisis
 					}
 				}
 			} else {
@@ -279,5 +298,9 @@ func main() {
 		} else {
 			fmt.Print("")
 		}
+	}
+	rename := os.Rename("output.csv", filename)
+	if rename != nil {
+		panic(rename)
 	}
 }
